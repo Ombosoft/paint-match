bac0941bf889af3763627d2a8a8edb895412aebc
Ombosoft <ombosoft@gmail.com>
2023-03-28 21:39:19 +0100

    Misc

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # paint-match
+
+This a color mixing game.
+
+To start, run
+
+`
+npm install
+npm start
+`
--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,5 @@
 export const extraCommitDelay = 150;
-export const skipLevels = 2;
+export const skipLevels = 0;
 export const animationDurationMs = 1500;
 export const dropletBlendDelay = 150;
 
