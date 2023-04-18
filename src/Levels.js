1e391e5f5c76a8ea389c1fa58d50b8d3710d5fe4
Ombosoft <ombosoft@gmail.com>
2023-04-18 20:53:36 +0100

    tune levels

--- src/Levels.js
+++ src/Levels.js
@@ -46,34 +46,33 @@ export const colorTable = [
     {name: "aqua", cmyk: [50, 0, 0, 20], tolerance: 4}, // level=49, complexity=14
     {name: "midnight blue", cmyk: [100, 100, 0, 80], tolerance: 4}, // level=50, complexity=14
     {name: "medium blue", cmyk: [100, 100, 0, 20], tolerance: 4}, // level=51, complexity=14
-    {name: "blue", cmyk: [60, 100, 0, 0], tolerance: 4}, // level=52, complexity=14
     {name: "true purple", cmyk: [0, 100, 0, 40], tolerance: 4}, // level=53, complexity=14
     {name: "broadway pink", cmyk: [0, 100, 60, 0], tolerance: 4}, // level=54, complexity=14
     {name: "gainsboro", cmyk: [0, 0, 0, 14], tolerance: 4}, // level=58, complexity=15
     {name: "dark orange", cmyk: [0, 45, 100, 0], tolerance: 5}, // level=60, complexity=15
     {name: "orange", cmyk: [0, 35, 100, 0], tolerance: 4}, // level=61, complexity=15
-    {name: "yellow", cmyk: [0, 0, 100, 45], tolerance: 4}, // level=62, complexity=15
+    {name: "dirty green", cmyk: [0, 0, 100, 45], tolerance: 4}, // level=62, complexity=15
     {name: "papaya", cmyk: [0, 0, 51, 0], tolerance: 4}, // level=63, complexity=15
     {name: "light yellow", cmyk: [0, 0, 12, 0], tolerance: 5}, // level=64, complexity=15
     {name: "ivory", cmyk: [0, 0, 6, 0], tolerance: 4}, // level=65, complexity=15
     {name: "dark cyan", cmyk: [100, 0, 0, 45], tolerance: 4}, // level=66, complexity=15
-    {name: "dark slate gray", cmyk: [41, 0, 0, 0], tolerance: 4}, // level=67, complexity=15
+    {name: "light blue", cmyk: [41, 0, 0, 0], tolerance: 4}, // level=67, complexity=15
     {name: "pale turquoise", cmyk: [27, 0, 0, 0], tolerance: 4}, // level=68, complexity=15
     {name: "light cyan", cmyk: [12, 0, 0, 0], tolerance: 4}, // level=69, complexity=15
     {name: "azure", cmyk: [6, 0, 0, 0], tolerance: 4}, // level=70, complexity=15
     {name: "dark magenta", cmyk: [0, 100, 0, 45], tolerance: 4}, // level=71, complexity=15
     {name: "plum", cmyk: [0, 27, 0, 0], tolerance: 4}, // level=72, complexity=15
     {name: "thistle", cmyk: [0, 12, 0, 0], tolerance: 4}, // level=73, complexity=15
-    {name: "blood red", cmyk: [0, 100, 100, 60], tolerance: 4}, // level=74, complexity=16
+    {name: "blood red", cmyk: [0, 100, 100, 60], tolerance: 1}, // level=74, complexity=16
     {name: "orange red", cmyk: [0, 75, 75, 0], tolerance: 5}, // level=75, complexity=16
     {name: "dark orange", cmyk: [0, 50, 100, 20], tolerance: 4}, // level=76, complexity=16
     {name: "chartreuse", cmyk: [50, 0, 100, 20], tolerance: 4}, // level=77, complexity=16
     {name: "irish flag", cmyk: [100, 0, 100, 40], tolerance: 4}, // level=78, complexity=16
     {name: "spring green", cmyk: [100, 0, 50, 20], tolerance: 4}, // level=79, complexity=16
-    {name: "dark red", cmyk: [0, 100, 100, 45], tolerance: 4}, // level=80, complexity=17
-    {name: "orange red", cmyk: [0, 86, 100, 0], tolerance: 4}, // level=81, complexity=17
-    {name: "orange red", cmyk: [0, 73, 100, 0], tolerance: 4}, // level=82, complexity=17
-    {name: "dark orange", cmyk: [0, 47, 100, 0], tolerance: 4}, // level=83, complexity=17
+    {name: "dark red", cmyk: [0, 100, 100, 45], tolerance: 2}, // level=80, complexity=17
+    {name: "orange red", cmyk: [0, 86, 100, 0], tolerance: 1}, // level=81, complexity=17
+    {name: "orange red", cmyk: [0, 73, 100, 0], tolerance: 1}, // level=82, complexity=17
+    {name: "dark orange", cmyk: [0, 47, 100, 0], tolerance: 3}, // level=83, complexity=17
     {name: "gold", cmyk: [0, 16, 100, 0], tolerance: 4}, // level=84, complexity=17
     {name: "yellow", cmyk: [0, 0, 100, 7], tolerance: 4}, // level=85, complexity=17
     {name: "green", cmyk: [100, 0, 100, 45], tolerance: 4}, // level=86, complexity=17
