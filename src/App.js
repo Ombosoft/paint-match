43ae9847048c8ace5bef08ca66fac6bcdc17fc19
Ombosoft <ombosoft@gmail.com>
2023-05-15 10:58:28 +0100

    extract MuteButtons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import { Stack } from "@mui/material";
 import './App.css';
 import Game from "./Game";
 import useMusic from "./Music";
-import { MuteButton } from "./MuteButtons";
+import { MuteButtons } from "./MuteButtons";
 
 
 // Main app component.
@@ -13,13 +12,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <Game autoPlayMusic={autoPlay} />
-                <Stack direction="row" justifyContent="end" sx={{
-                    position: 'fixed',
-                    bottom: "0em",
-                    left: "0em",
-                }}>
-                    <MuteButton muted={muted} toggleMute={toggleMute}/>
-                </Stack>
+                <MuteButtons muted={muted} toggleMute={toggleMute}/>
             </header>
         </div>
     );
--- src/MuteButtons.js
+++ src/MuteButtons.js
@@ -2,8 +2,21 @@ import MusicNoteIcon from '@mui/icons-material/MusicNote';
 import MusicOffIcon from '@mui/icons-material/MusicOff';
 import { IconButton, Tooltip } from "@mui/material";
 import React from "react";
+import { Stack } from "@mui/material";
 
-export function MuteButton({ muted, toggleMute }) {
+export function MuteButtons({ muted, toggleMute }) {
+    return (
+        <Stack direction="row" justifyContent="end" sx={{
+            position: 'fixed',
+            bottom: "0em",
+            left: "0em",
+        }}>
+            <MuteButton muted={muted} toggleMute={toggleMute} />
+        </Stack>);
+}
+
+
+function MuteButton({ muted, toggleMute }) {
     return (<>
         <Tooltip title={muted ? "Play music" : "Mute music"} placement="top-end" arrow>
             <IconButton
