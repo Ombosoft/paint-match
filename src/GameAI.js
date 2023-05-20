c33a6b30865157b0df29ddda22a061b58e409519
Ombosoft <ombosoft@gmail.com>
2023-05-20 20:24:25 +0100

    optimize

--- src/GameAI.js
+++ src/GameAI.js
@@ -1,12 +1,30 @@
 import colorDistance from "./ColorDistance";
 import { blendPaints, zeroComponents } from "./Colors";
+import { range } from "./Util/Utils";
 import { objectValueSum } from "./Util/Vec";
+
+function basisComponents(dest) {
+    let result = ['white', 'black'];
+    const colorKeys = ["red", "green", "blue", "cyan", "magenta", "yellow"];
+    for (const componentName of colorKeys) {
+        let probe = { ...zeroComponents };
+        probe[componentName] = 1;
+        const compCmyk = blendPaints(probe);
+        for (let i in range(4)) {
+            if (compCmyk[i] !== 0 && dest[i] !== 0) {
+                result.push(componentName);
+                break;
+            }
+        }
+    }
+    return result;
+}
 
 // source: array[CMYK] -> dest: array[CMYK] -> eps: number -> object{components}
 export function optimalPath(dest, eps, maxCost = 13) {
     const source = [0, 0, 0, 0];
     console.assert(Array.isArray(dest));
-    const componentNames = Object.keys(zeroComponents);
+    const componentNames = basisComponents(dest);
     let buf = [{ cmyk: source, comps: zeroComponents }];
     // comps (path) -> bool
     let visited = {};
@@ -26,9 +44,7 @@ export function optimalPath(dest, eps, maxCost = 13) {
             }
             const nextCMYK = blendPaints(nextComps);
             const nextCompsKey = JSON.stringify(nextComps);
-            if (
-                visited[nextCompsKey]
-            ) {
+            if (visited[nextCompsKey]) {
                 // console.log('visited', nextComps);
                 continue;
             }
