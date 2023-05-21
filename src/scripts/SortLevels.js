8f3c45287c9889d70216a848e3a2a65561a9d881
Ombosoft <ombosoft@gmail.com>
2023-05-21 13:18:18 +0100

    refactor sortlevels

--- src/scripts/SortLevels.js
+++ src/scripts/SortLevels.js
@@ -3,19 +3,36 @@ import { minCost } from "../GameAI.js";
 import { colorTable } from "../Levels.js";
 const fs = require("fs");
 
-let code = "export const colorTable = [\n";
+function save(levels) {
+    let code =
+        "export const colorTable = [\n" +
+        levels
+            .map(
+                (level) =>
+                    `    { index: ${level.index}, name: "${level.name}", cmyk: ${level.cmykStr}, ` +
+                    `cost: ${level.cost}, tolerance: ${level.tolerance}${level.ewt} },\n`
+            )
+            .join("") +
+        "];\n";
+    fs.writeFileSync("src/Levels.js", code);
+}
+
 let index = 0;
 for (const level of colorTable) {
-    const cmyk = JSON.stringify(level.cmyk);
+    level.cmykStr = JSON.stringify(level.cmyk);
     const cost = minCost(level.cmyk, getWinTolerance(level));
     console.log(index, cost);
     let extraWinTolerance = level.extraWinTolerance ?? 0;
     if (cost === 0) {
         extraWinTolerance += 0.1;
     }
-    const ewt = extraWinTolerance ? `, extraWinTolerance: ${extraWinTolerance.toFixed(1)}` : '';
-    code += `    { index: ${index}, name: "${level.name}", cmyk: ${cmyk}, cost: ${cost}, tolerance: ${level.tolerance}${ewt} },\n`;
+    const ewt = extraWinTolerance
+        ? `, extraWinTolerance: ${extraWinTolerance.toFixed(1)}`
+        : "";
+    level.cost = cost;
+    level.ewt = ewt;
+    level.index = index;
     index += 1;
 }
-code += "];\n";
-fs.writeFileSync("src/Levels.js", code);
+
+save(colorTable);
