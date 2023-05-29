53272f0ece7c8226461c7521eec77b41ac97e5ea
Ombosoft <ombosoft@gmail.com>
2023-05-29 18:37:27 +0100

    Blinking cursor

--- src/Components/HintBox.js
+++ src/Components/HintBox.js
@@ -1,30 +1,36 @@
 import { Box } from "@mui/material";
 import { useEffect, useState } from "react";
 import { hintTypingDelay } from "../Constants";
+import Cursor from "./Cursor";
 
 export function HintBox({ hint }) {
     const [words, setWords] = useState(0);
-    
+    const animationActive = hint !== null && words < hint.length;
+
     useEffect(() => {
-        if (hint === null || words >= hint.length) {
+        if (!animationActive) {
             return;
         }
         const timerId = setTimeout(() => {
-            setWords(prev => prev + 1);
+            setWords((prev) => prev + 1);
         }, hintTypingDelay);
 
         return () => {
             clearTimeout(timerId);
         };
-    }, [hint, words]);
-    
+    }, [animationActive, setWords, words]);
+
     // Hack to refresh tooltip position
     useEffect(() => {
-        window.dispatchEvent(new Event('resize'));
+        window.dispatchEvent(new Event("resize"));
     }, [words]);
-    
+
     if (hint === null) {
-        return (<></>);
+        return <></>;
     }
-    return <Box>{hint.slice(0, words)}</Box>;
+    return (
+        <Box>
+            {hint.slice(0, words)} {animationActive && <Cursor />}
+        </Box>
+    );
 }
