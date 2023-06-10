c65e7655ed80bfbe3adab9b66f1f74620e1823c2
Ombosoft <ombosoft@gmail.com>
2023-06-10 22:07:29 +0100

    better victory chart font

--- src/Components/PaletteChart.js
+++ src/Components/PaletteChart.js
@@ -15,72 +15,10 @@ const theme = {
         outlineWidth: 6,
         outlineColor: "#da1010",
     },
-    axis: {
-        domain: {
-            line: {
-                stroke: "#777777",
-                strokeWidth: 1,
-            },
-        },
-        legend: {
-            text: {
-                fontSize: 12,
-                fill: "#333333",
-                outlineWidth: 0,
-                outlineColor: "transparent",
-            },
-        },
-        ticks: {
-            line: {
-                stroke: "#777777",
-                strokeWidth: 1,
-            },
-            text: {
-                fontSize: 11,
-                fill: "#333333",
-                outlineWidth: 0,
-                outlineColor: "transparent",
-            },
-        },
-    },
-    grid: {
-        line: {
-            stroke: "#dddddd",
-            strokeWidth: 1,
-        },
-    },
-    legends: {
-        title: {
-            text: {
-                fontSize: 11,
-                fill: "#333333",
-                outlineWidth: 0,
-                outlineColor: "transparent",
-            },
-        },
-        text: {
-            fontSize: 111,
-            fill: "#333333",
-            outlineWidth: 0,
-            outlineColor: "transparent",
-        },
-        ticks: {
-            line: {},
-            text: {
-                fontSize: 10,
-                fill: "#333333",
-                outlineWidth: 0,
-                outlineColor: "transparent",
-            },
-        },
-    },
     labels: {
         text: {
-            fontSize: "1em",
-            fill: "#333333",
-            outlineWidth: 20,
-            outlineColor: "#ffffff",
-            outlineOpacity: 1,
+            fontSize: "1.5em",
+            fontWeight: "bold",
         },
     },
     annotations: {
