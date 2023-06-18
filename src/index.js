86846f025e7b962cc529f09bfe5056ab9123f686
Ombosoft <ombosoft@gmail.com>
2023-06-18 15:52:51 +0100

    use sentry

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
+import * as Sentry from "@sentry/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ErrorBoundary } from "react-error-boundary";
 import App from "./Components/App";
 import { gaInit } from "./GA";
 import "./index.css";
 
+Sentry.init({
+    dsn: "https://7ba852b4802048b191a9deda0dc08547@o4505380972265472.ingest.sentry.io/4505380974886912",
+    integrations: [
+      new Sentry.Replay(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  });
+
+  
 gaInit();
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const logError = (error, info) => {
-    console.error({ error }, { info });
-};
 root.render(
     <React.StrictMode>
-        <ErrorBoundary
-            fallback={<div>Something went wrong</div>}
-            onError={logError}
-        >
-            <App />
-        </ErrorBoundary>
+        <App />
     </React.StrictMode>
 );
