f9ec1ae28d125eea53c28b8c50ec620cf928b081
Ombosoft <ombosoft@gmail.com>
2023-06-19 21:48:23 +0100

    extract sentry config

--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
-import * as Sentry from "@sentry/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./Components/App";
 import { gaInit } from "./GA";
+import sentryInit from "./Sentry";
 import "./index.css";
 
-Sentry.init({
-    dsn: "https://7ba852b4802048b191a9deda0dc08547@o4505380972265472.ingest.sentry.io/4505380974886912",
-    integrations: [
-      new Sentry.Replay(),
-    ],
-    // Performance Monitoring
-    tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
-    // Session Replay
-    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-  });
-
-  
+sentryInit();
 gaInit();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
