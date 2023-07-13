ee651a066305551f7490f61c4cc18010e52d4693
Ombosoft <ombosoft@gmail.com>
2023-07-13 21:17:41 +0100

    update version

--- src/Sentry.js
+++ src/Sentry.js
@@ -10,7 +10,7 @@ export function sentryInit() {
         }
         Sentry.init({
             environment: process.env.NODE_ENV,
-            release: "v9.1",
+            release: "10",
             dsn: sentryDsn,
             integrations: [
                 new Sentry.Replay({
@@ -19,10 +19,10 @@ export function sentryInit() {
                 }),
             ],
             // Performance Monitoring
-            tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+            tracesSampleRate: 0.5,
             // Session Replay
-            replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-            replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+            replaysSessionSampleRate: 0.01,
+            replaysOnErrorSampleRate: 1.0,
         });
     } catch {}
 }
