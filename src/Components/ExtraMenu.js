1d6bc0ab965b04c8d6f15702d244c6cfb88d0f23
Ombosoft <ombosoft@gmail.com>
2023-07-15 20:49:50 +0100

    Rate on itch snack

--- src/Components/ExtraMenu.js
+++ src/Components/ExtraMenu.js
@@ -1,51 +1,73 @@
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import BugReportIcon from "@mui/icons-material/BugReport";
 import PersonPinIcon from "@mui/icons-material/PersonPin";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
-import { Stack } from "@mui/material";
+import { Box, Snackbar, Stack } from "@mui/material";
 import PropTypes from "prop-types";
-import { textForeground } from "../Constants";
+import { useState } from "react";
+import { imgPath, textForeground } from "../Constants";
 import CaptionButton from "./CaptionButton";
 
 function ExtraMenu({ onCredits, onFeedback }) {
+    const [rateOpen, setRateOpen] = useState(false);
+    function onRate() {
+        setRateOpen(true);
+    }
     return (
-        <Stack
-            direction="row"
-            spacing={2}
-            sx={{
-                zIndex: 1,
-                fontSize: "2em",
-                background: "rgba(0, 0, 0, 0.6)",
-                borderRadius: "21px",
-                justifyContent: "center",
-                paddingTop: "0.5em",
-                paddingBottom: "0.3em",
-            }}
-        >
-            <CaptionButton
-                id="rate"
-                caption="Rate Us"
-                captionColor={textForeground}
-                onClick={onCredits}
+        <>
+            <Stack
+                direction="row"
+                spacing={2}
+                sx={{
+                    zIndex: 1,
+                    fontSize: "2em",
+                    background: "rgba(0, 0, 0, 0.6)",
+                    borderRadius: "21px",
+                    justifyContent: "center",
+                    paddingTop: "0.5em",
+                    paddingBottom: "0.3em",
+                }}
             >
-                <ThumbUpIcon />
-            </CaptionButton>
-            <CaptionButton
-                id="feedback"
-                caption="Feedback"
-                captionColor={textForeground}
-                onClick={onFeedback}
-            >
-                <BugReportIcon />
-            </CaptionButton>
-            <CaptionButton
-                id="credits"
-                caption="Credits"
-                captionColor={textForeground}
-                onClick={onCredits}
-            >
-                <PersonPinIcon />
-            </CaptionButton>
-        </Stack>
+                <CaptionButton
+                    id="rate"
+                    caption="Rate Us"
+                    captionColor={textForeground}
+                    onClick={onRate}
+                >
+                    <ThumbUpIcon />
+                </CaptionButton>
+                <CaptionButton
+                    id="feedback"
+                    caption="Feedback"
+                    captionColor={textForeground}
+                    onClick={onFeedback}
+                >
+                    <BugReportIcon />
+                </CaptionButton>
+                <CaptionButton
+                    id="credits"
+                    caption="Credits"
+                    captionColor={textForeground}
+                    onClick={onCredits}
+                >
+                    <PersonPinIcon />
+                </CaptionButton>
+            </Stack>
+            <Snackbar
+                open={rateOpen}
+                autoHideDuration={5000}
+                onClose={() => {
+                    setRateOpen(false);
+                }}
+                message={
+                    <Stack direction="row" alignItems="center" spacing={1}>
+                        <Box fontSize="1.5em">Please rate us on itch.io</Box>
+                        <ArrowForwardIcon/>
+                        <img src={imgPath("rate-itch.png")} alt="" />
+                    </Stack>
+                }
+            ></Snackbar>
+        </>
     );
 }
 
