38f3346d61baed1dc44d91e46dba42ecd346acfa
Ombosoft <ombosoft@gmail.com>
2023-07-21 23:18:51 +0100

    version bump

--- src/Sentry.js
+++ src/Sentry.js
@@ -10,7 +10,7 @@ export function sentryInit() {
         }
         Sentry.init({
             environment: process.env.NODE_ENV,
-            release: "11",
+            release: "11.1",
             dsn: sentryDsn,
             integrations: [
                 new Sentry.Replay({
