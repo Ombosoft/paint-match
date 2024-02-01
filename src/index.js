d94be6ccde50268dff9028de0eb4cbf71a73b061
Ombosoft <ombosoft@gmail.com>
2024-02-01 20:50:26 +0000

    disable sentry

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./Components/App";
 import { gaInit } from "./GA";
 import { nativeInit } from "./Native";
-import { sentryInit } from "./Sentry";
 import "./index.css";
 
 if (process.env.NODE_ENV === "production") {
-    sentryInit();
     gaInit();
 }
 
